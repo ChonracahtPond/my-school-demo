cf43de01840a2729be58c42823deff3afb85d87c
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-10-09 15:05:53 +0700

    pond

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -250,21 +250,7 @@ export default function Regfrom() {
             </select>
           </div>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-            {/* <div>
-              <label className="text-white dark:text-gray-200" htmlFor="fname">ชื่อ</label>
-              <input id="fname" value={fname} onChange={(e) => setfname(e.target.value)} type="text"  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
-            </div> */}
-            {/* <div>
-              <label className="text-white dark:text-gray-200" htmlFor="fname">ชื่อ</label>
-              <input
-                id="fname"
-                value={fname}
-                onChange={(e) => setfname(e.target.value)}
-                type="text"
-                pattern="^[A-Za-zก-๙\s]*$" title="กรุณากรอกตัวอักษรเท่านั้น" required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div> */}
+           
             <div>
               <label className="text-white dark:text-gray-200" htmlFor="fname">ชื่อ</label>
               <input
