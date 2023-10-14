b9fce59cf38477d2c9d6e6ba4ee199fe255d9dfa
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-10-15 02:08:59 +0700

    pond

--- src/pages/api/regform/index.ts
+++ src/pages/api/regform/index.ts
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-=======
-// import { PrismaClient } from "@prisma/client";
-// import type { NextApiRequest, NextApiResponse } from 'next';
-
-// const prisma = new PrismaClient();
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//     const { method } = req;
-
-//     switch (method) {
-//         case 'GET':
-//             try {
-//                 const page: number = Number(req.query.page) || 1;
-//                 const pageSize: number = Number(req.query.pageSize) || 10;
-
-//                 const regform = await prisma.regform.findMany({
-//                     skip: (page - 1) * pageSize,
-//                     take: pageSize,
-//                 });
-
-//                 const totalregform = await prisma.regform.count();
-//                 const totalPage: number = Math.ceil(totalregform / pageSize);
-//                 res.status(200).json({ regform });
-//             } catch (error) {
-//                 res.status(500).json({ error: "An error occurred while fetching the regform" });
-//             }
-//             break;
-
-//         case 'POST':
-//             try {
-//                 const newregform = await prisma.regform.create({
-//                     data: req.body,
-//                 });
-
-//                 res.status(201).json(newregform);
-//             } catch (error) {
-//                 res.status(500).json({ error: "An error occurred while creating the regform" });
-//             }
-//             break;
-
-//         default:
-//             res.setHeader('Allow', ['GET', 'POST']);
-//             res.status(405).end(`Method ${method} Not Allowed`);
-//     }
-// }
-
->>>>>>> 9c097bba6596d653f617c9ccacf643ab4ce5b61e
 
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -61,30 +13,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 const page: number = Number(req.query.page) || 1;
                 const pageSize: number = Number(req.query.pageSize) || 10;
 
-                const regform = await prisma.regform.findMany({
+                const regfrom = await prisma.regfrom.findMany({
                     skip: (page - 1) * pageSize,
                     take: pageSize,
                 });
 
-                const totalregform = await prisma.regform.count();
-                const totalPage: number = Math.ceil(totalregform / pageSize);
-                res.status(200).json({ regform, totalPage });
+                const totalregfrom = await prisma.regfrom.count();
+                const totalPage: number = Math.ceil(totalregfrom / pageSize);
+                res.status(200).json({ regfrom, totalPage });
             } catch (error) {
                 console.error(error);
-                res.status(500).json({ error: "An error occurred while fetching the regform" });
+                res.status(500).json({ error: "An error occurred while fetching the regfrom" });
             }
             break;
 
         case 'POST':
             try {
-                const newregform = await prisma.regform.create({
+                const newregfrom = await prisma.regfrom.create({
                     data: req.body,
                 });
 
-                res.status(201).json(newregform);
+                res.status(201).json(newregfrom);
             } catch (error) {
                 console.error(error);
-                res.status(500).json({ error: "An error occurred while creating the regform" });
+                res.status(500).json({ error: "An error occurred while creating the regfrom" });
             }
             break;
 
